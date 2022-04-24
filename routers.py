01444b45ec0c0aa0d042e25371ae4104be3e1a24
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-04-24 18:00:27 +0200

    Add data_source to get_building endpoint

--- routers.py
+++ routers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Response
 from httpx import AsyncClient
 
+from model import BuildingsDataSource
+
+
 router_v1 = APIRouter()
 
 
@@ -8,17 +11,29 @@ router_v1 = APIRouter()
 async def get_building(
         lat: float,
         lon: float,
+        data_source: BuildingsDataSource,
         search_distance: float = 0.1
 ):
     """
     :param lat EPSG4386
     :param lon EPSG4386
+    :param data_source source from which will be data obtained
     :param search_distance radius in meters
     """
+
     async with AsyncClient() as client:
-        response = await client.get(
-            'https://budynki.openstreetmap.org.pl'
-            '/josm_plugins/nearest_building'
-            f'?lon={lon}&lat={lat}&search_distance={search_distance}'
+        if data_source == BuildingsDataSource.BDOT:
+            response = await client.get(
+                'https://budynki.openstreetmap.org.pl'
+                '/josm_plugins/nearest_building'
+                f'?lon={lon}&lat={lat}&search_distance={search_distance}'
+            )
+            return Response(
+                content=response.text,
+                media_type='application/xml'
+            )
+
+        return Response(
+            content='<osm version="0.6"/>',
+            media_type='application/xml'
         )
-        return Response(content=response.text, media_type='application/xml')
