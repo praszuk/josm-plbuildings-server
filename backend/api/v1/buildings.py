ed7868d1189fac675039a6d6423afaf3eeb56bdb
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-05-21 01:12:24 +0200

    Restructurize app files.
    Move routers/models to specific modules to keep it simple and clean.

--- backend/api/v1/buildings.py
+++ backend/api/v1/buildings.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Response
 from httpx import AsyncClient
 
-from model import BuildingsDataSource
+from backend.models.enums import BuildingsDataSource
 
 
-router_v1 = APIRouter()
+router = APIRouter()
 
 
-@router_v1.get('/buildings')
+@router.get('')
 async def get_nearest_building(
         lat: float,
         lon: float,
         data_source: BuildingsDataSource,
-        search_distance: float = 3
+        search_distance: float = 3,
 ):
     """
     :param lat EPSG4386
@@ -20,14 +20,14 @@ async def get_nearest_building(
     :param data_source source from which will be data obtained
     :param search_distance radius in meters
     """
-
     async with AsyncClient() as client:
         if data_source == BuildingsDataSource.BDOT:
             response = await client.get(
-                'http://budynki.openstreetmap.org.pl'
+                'https://budynki.openstreetmap.org.pl'
                 '/josm_plugins/nearest_building'
                 f'?lon={lon}&lat={lat}&search_distance={search_distance}'
             )
+
             return Response(
                 content=response.text,
                 media_type='application/xml'
