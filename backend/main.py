d98f1d128a1ad7688dc33bd28ec5bf1ecfe77ba7
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-10-30 00:01:23 +0200

    Add v2 api_router

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from backend.api.v1.api import api_router
+from backend.api.v1.api import api_router as api_router_v1  # deprecated
+from backend.api.v2.api import api_router as api_router_v2
 from backend.core.config import settings
 
 app = FastAPI()
-app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router_v1, prefix=settings.API_V1_STR)  # deprecated
+app.include_router(api_router_v2, prefix=settings.API_V2_STR)
