b16cae7ab800b253c6fb564aee3e874126301cef
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-01-31 00:08:52 +0100

    Add DataSourceFormat

--- backend/models/enums.py
+++ backend/models/enums.py
@@ -4,3 +4,8 @@ from enum import Enum
 class BuildingsDataSource(str, Enum):
     BDOT = 'bdot'  # From 'budynki' site
     EGIB = 'egib'  # From WFSes gugik.gov.pl services (in feature)
+
+
+class DataSourceFormat(Enum):
+    GEOJSON = 'geojson'
+    OSMJSON = 'osmjson'
