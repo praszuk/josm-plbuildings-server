32c62c095f5ad61cf2730594eb831c95c1ad595e
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-01-31 00:13:51 +0100

    Update BuildingsService to use async BDOT service

--- backend/services/buildings.py
+++ backend/services/buildings.py
@@ -1,53 +1,60 @@
+import asyncio
+
 from httpx import AsyncClient
 
 from datetime import datetime
+from typing import List, Optional
 
 from backend.api.v2.deps import BuildingsNearestParams
-from backend.core.config import settings
 from backend.crud.buildings_log import create_buildings_log
 from backend.models.enums import BuildingsDataSource
+from backend.schemas.buildings_data import BuildingsData
 from backend.schemas.buildings_log import BuildingsLogCreate
 from backend.services.base import BaseService
-from backend.utils import osm_parsers
+from backend.services.bdot import BDOTService
 
 
 class BuildingsService(BaseService):
-    async def get_nearest_building(self, nearest: BuildingsNearestParams):
+    async def get_nearest_building(
+        self,
+        nearest: BuildingsNearestParams
+    ) -> List[Optional[BuildingsData]]:
+
         request_receive_dt = datetime.utcnow()
 
-        result_data = {}
-        # empty_osm_data = '<osm version="0.6"/>'
+        result_buildings_data = []
+        building_count = 0
 
+        services = []
         async with AsyncClient() as client:
-            building_count = 0
-            if BuildingsDataSource.BDOT.value in nearest.data_sources:
-                response = await client.get(
-                    f'{settings.BUDYNKI_SERVER_URL}'
-                    '/josm_plugins/nearest_building'
-                    f'?lon={nearest.lon}'
-                    f'&lat={nearest.lat}'
-                    f'&search_distance={nearest.search_distance}'
-                )
-                result_data[BuildingsDataSource.BDOT.value] = response.text
-                building_count = osm_parsers.count_buildings(response.text)
+            if BuildingsDataSource.BDOT in nearest.data_sources:
+                services.append(BDOTService(client))
 
-            request_timedelta = datetime.utcnow() - request_receive_dt
-            request_duration_ms = request_timedelta.total_seconds() * 1000
-
-            if BuildingsDataSource.EGIB.value in nearest.data_sources:
-                # TODO
+            if BuildingsDataSource.EGIB in nearest.data_sources:
+                # services.append(EGIBService(client)) TODO
                 pass
 
-            create_buildings_log(
-                self.db,
-                buildings_log=BuildingsLogCreate(
-                    rq_recv_dt=request_receive_dt,
-                    rq_duration_ms=request_duration_ms,
-                    lat=nearest.lat,
-                    lon=nearest.lon,
-                    data_sources=nearest.data_sources,
-                    building_count=building_count,
-                )
+            await asyncio.gather(
+                *[s.fetch_nearest_building(nearest) for s in services]
             )
 
-        return result_data
+            for service in services:
+                result_buildings_data.append(service.buildings_data)
+                building_count += service.buildings_count
+
+        request_timedelta = datetime.utcnow() - request_receive_dt
+        request_duration_ms = request_timedelta.total_seconds() * 1000
+
+        create_buildings_log(
+            self.db,
+            buildings_log=BuildingsLogCreate(
+                rq_recv_dt=request_receive_dt,
+                rq_duration_ms=request_duration_ms,
+                lat=nearest.lat,
+                lon=nearest.lon,
+                data_sources=nearest.data_sources,
+                building_count=building_count,
+            )
+        )
+
+        return result_buildings_data
