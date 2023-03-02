ad85147254bd4b67c8709ad8a269fc87d57c32df
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-03-02 17:14:37 +0100

    Add flatten_query_stirng_list middleware
    It fixes passing multiple values (as list) in URL.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from urllib.parse import urlencode
 
 from backend.api.v1.api import api_router as api_router_v1  # deprecated
 from backend.api.v2.api import api_router as api_router_v2
@@ -7,3 +8,24 @@ from backend.core.config import settings
 app = FastAPI()
 app.include_router(api_router_v1, prefix=settings.API_V1_STR)  # deprecated
 app.include_router(api_router_v2, prefix=settings.API_V2_STR)
+
+
+@app.middleware('http')
+async def flatten_query_string_lists(request: Request, call_next):
+    """
+    Allow to pass list arguments like this:
+    - &param=1,2,3
+    instead of doing this:
+    - &param=1&param=2&param=3
+    https://github.com/tiangolo/fastapi/issues/50#issuecomment-1042362633
+    """
+    flattened = []
+    for key, value in request.query_params.multi_items():
+        flattened.extend((key, entry) for entry in value.split(','))
+
+    request.scope['query_string'] = urlencode(
+        flattened,
+        doseq=True
+    ).encode('utf-8')
+
+    return await call_next(request)
