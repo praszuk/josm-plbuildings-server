cf938bd6f220bee908f79f14e4ddb7469882105a
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-07-15 15:54:56 +0200

    Fix comments

--- backend/models/enums.py
+++ backend/models/enums.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class BuildingsDataSource(str, Enum):
-    BDOT = 'bdot'  # From 'budynki' site
-    EGIB = 'egib'  # From WFSes gugik.gov.pl services
+    BDOT = 'bdot'  # From 'budynki' server
+    EGIB = 'egib'  # From 'egib-plbuildings' server
 
 
 class DataSourceFormat(Enum):
